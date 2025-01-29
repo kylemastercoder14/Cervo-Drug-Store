f0328ce9176bb37c2c16f62f639676942ddcca69
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-01-29 13:50:07 +0800

    completed

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["cervo-drugstore.s3.us-east-1.amazonaws.com", "coffeeprince.s3.us-east-1.amazonaws.com"]
-    }
+  images: {
+    domains: [
+      "cervo-drugstore.s3.us-east-1.amazonaws.com",
+      "coffeeprince.s3.us-east-1.amazonaws.com",
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
