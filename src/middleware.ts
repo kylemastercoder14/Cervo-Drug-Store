ac8c3f151fa0ea6cffe6368990aae79984d674ab
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-01-29 15:26:40 +0800

    completed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,26 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/my-account', '/senior-and-pwd-account-registration', '/verify-email', '/admin/(.*)', '/', '/admin'])
+const isPublicRoute = createRouteMatcher([
+  "/my-account",
+  "/senior-and-pwd-account-registration",
+  "/verify-email",
+  "/admin/(.*)",
+  "/",
+  "/admin",
+  "/admin/products",
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
-    auth().protect()
+    auth().protect();
   }
-})
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+};
