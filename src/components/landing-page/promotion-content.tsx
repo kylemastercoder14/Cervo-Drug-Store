54ebdb05a4795e8e7963bcf44eaa0deb711bae0f
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-02-20 22:44:28 +0800

    updated height promotions

--- src/components/landing-page/promotion-content.tsx
+++ src/components/landing-page/promotion-content.tsx
@@ -24,12 +24,12 @@ const PromotionContent = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-6">
       {promotions.map((item) => (
-        <div key={item.id} className="relative w-full h-[400px]">
+        <div key={item.id} className="relative w-full h-[600px]">
           <Image
             src={item.image}
             alt={item.image}
             fill
-            className="w-full h-full rounded-xl object-cover"
+            className="w-full h-full rounded-xl object-cover shadow-md"
           />
         </div>
       ))}
