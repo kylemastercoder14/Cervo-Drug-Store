80a2b687235647bd97fa6c2e75545d92612962e5
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-03-07 16:55:37 +0800

    fixed null issue

--- src/app/admin/(pages)/inventory/_components/client.tsx
+++ src/app/admin/(pages)/inventory/_components/client.tsx
@@ -24,10 +24,10 @@ const InventoryClient = () => {
   const formattedData: InventoryColumn[] =
     inventoryData?.data?.map((item) => ({
       id: item.id,
-      image: item.product.image,
+      image: item.product.image || "",
       name: item.product.name,
       productId: item.productId,
-      tags: item.product.tags,
+      tags: item.product.tags || "",
       status: item.quantity > 0 ? "In Stock" : "Out of Stock",
       stock: item.quantity,
       createdAt: format(item.createdAt, "MMMM do, yyyy"),
