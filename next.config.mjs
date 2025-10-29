45f3edda00548f342eac5eb1aa6d2372ada11574
jeffreelingal011 <jeffreelingal00@gmail.com>
2025-10-29 12:30:50 +0800

    fixed issues

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: [
       "cervo-drugstore.s3.us-east-1.amazonaws.com",
+      "cervo-drugstore-production.s3.ap-southeast-2.amazonaws.com",
       "coffeeprince.s3.us-east-1.amazonaws.com",
       "southstardrug.com.ph",
     ],
